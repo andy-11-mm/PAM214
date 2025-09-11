f0d59920b4824c4f322d0e8cee5ea3a6dd196108
andy-11-mm <124050194@upq.edu.mx>
2025-09-10 18:50:22 -0600

    video classroom
    avance del código de java para principiantes

---
+++ Untitled-1.java
@@ -0,0 +1,25 @@
+//tipos de datos
+"Hello word" // string
+"hello word" // string
+
+1000000 //number
+-2, 3
+
+//boolean
+true
+false
+
+//array
+[ 'joe', 'ryan', 'martha']
+[1,2,3]
+[ true, false, true, false]
+
+
+// object
+{
+    "username": 'ryan';
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": [ '']
+}
